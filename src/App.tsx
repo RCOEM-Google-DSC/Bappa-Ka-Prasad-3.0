1a233e1d8a2d8ed26d4453b96c2d71f43a111a76
Adnan Shabbir Husain <generaladnan139@gmail.com>
2023-09-08 18:12:05 +0530

    middle section complete

--- src/App.tsx
+++ src/App.tsx
@@ -8,23 +8,31 @@ import Navbar from "./components/navbar";
 
 function App() {
   return (
-    <>
+    <div className="main-bg">
       <Navbar />
       <div className="w-screen flex flex-col px-5 justify-center items-center">
         <LandingSection />
         <AboutSection />
         <MiddleSection
-          title="Prizes"
-          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis corrupti asperiores, re."
+          title="Why Sponsor Us?"
+          content={[
+            { title: "Talent Pool", image: "/midsection/talent-pools.png" },
+            { title: "Brand Awareness", image: "/midsection/brand-awareness-V1.5.png" },
+            { title: "Promotion", image: "/midsection/Vector-Promotion.jpg" },
+          ]}
         />
         <MiddleSection
-          title="Our Partners"
-          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis corrupti asperiores, re."
+          title="Why Partnership with us?"
+          content={[
+            { title: "Adding value to Community", image: "/midsection/value.jpeg" },
+            { title: "Promotion", image: "/midsection/Vector-Promotion.jpg" },
+            { title: "Community Reach", image: "/midsection/community-reach.png" },
+          ]}
         />
         <LeaderBoardSection />
       </div>
       <FooterSection />
-    </>
+    </div>
   );
 }
 
