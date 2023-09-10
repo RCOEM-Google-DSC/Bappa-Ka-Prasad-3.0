820f2676724d4336b3d7b70cef94256d9beb3644
sanchit-pro <san.tech721@gmail.com>
2023-09-10 18:54:31 +0530

    Added leaderboard

--- src/components/leaderboard.tsx
+++ src/components/leaderboard.tsx
@@ -1,7 +1,9 @@
+import leaderboard from '/leaderboard.svg';
 export default function LeaderBoardSection() {
   return (
     <div>
-      <p>LeaderBoard</p>
+      <p className='mid-section-title text-center text-lg mt-16 mb-12 sm:text-xl sm:mt-12 sm:mb-16 sm:mt-0 md:text-2xl lg:text-5xl lg:mt-16 lg:mb-20'>Global Leaderboard 2022</p>
+      <img className='w-[1000px]' src={leaderboard} alt="Leaderboard"/>
     </div>
   );
 }
