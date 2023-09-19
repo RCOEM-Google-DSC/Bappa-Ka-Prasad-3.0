f2abf2941931cc422c2511c15fefbac0b3781f55
EvolutionX <85353424+EvolutionX-10@users.noreply.github.com>
2023-09-19 21:37:48 +0530

    chore: update to use yarn 3 and style the files

--- src/App.tsx
+++ src/App.tsx
@@ -11,57 +11,74 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBSqRE6ZqZ8l1aKZCB9Ia_LsK_4yYANw5M",
-  authDomain: "bkprashad-3.firebaseapp.com",
-  projectId: "bkprashad-3",
-  storageBucket: "bkprashad-3.appspot.com",
-  messagingSenderId: "709644961830",
-  appId: "1:709644961830:web:b12967a60fc61557873ee7",
-  measurementId: "G-5HQV0X6RPH",
+	apiKey: "AIzaSyBSqRE6ZqZ8l1aKZCB9Ia_LsK_4yYANw5M",
+	authDomain: "bkprashad-3.firebaseapp.com",
+	projectId: "bkprashad-3",
+	storageBucket: "bkprashad-3.appspot.com",
+	messagingSenderId: "709644961830",
+	appId: "1:709644961830:web:b12967a60fc61557873ee7",
+	measurementId: "G-5HQV0X6RPH",
 };
 
 const app = initializeApp(firebaseConfig);
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const analytics = getAnalytics(app);
+getAnalytics(app);
 
 AOS.init();
 
 function App() {
-  return (
-    <div className="main-bg">
-      <Navbar />
-      <div className="w-screen flex flex-col px-5 justify-center items-center overflow-hidden">
-        <LandingSection />
-        <AboutSection />
-        <MiddleSection
-          title="Why Sponsor Us?"
-          content={[
-            { title: "Talent Pool", image: "/midsection/talent-pools.svg" },
-            { title: "Brand Awareness", image: "/midsection/brand-awareness.svg" },
-            { title: "Promotion", image: "/midsection/promotion.svg" },
-          ]}
-        />
-        <MiddleSection
-          title="Why Partnership with us?"
-          content={[
-            { title: "Adding value to Community", image: "/midsection/value.svg" },
-            { title: "Promotion", image: "/midsection/promotion.svg" },
-            { title: "Community Reach", image: "/midsection/community-reach.svg" },
-          ]}
-        />
-        <a
-          target="_blank"
-          className="px-4 py-3 bg-[#BE5038] text-white font-semibold transition-colors rounded-md hover:bg-opacity-80 xl:text-xl lg:text-xl md:text-lg"
-          rel="noreferrer"
-          href="https://forms.gle/ChwWq6JdxWjCeerg7"
-        >
-          Apply As Partner
-        </a>
-        <LeaderBoardSection />
-      </div>
-      <FooterSection />
-    </div>
-  );
+	return (
+		<div className="main-bg">
+			<Navbar />
+			<div className="w-screen flex flex-col px-5 justify-center items-center overflow-hidden">
+				<LandingSection />
+				<AboutSection />
+				<MiddleSection
+					title="Why Sponsor Us?"
+					content={[
+						{
+							title: "Talent Pool",
+							image: "/midsection/talent-pools.svg",
+						},
+						{
+							title: "Brand Awareness",
+							image: "/midsection/brand-awareness.svg",
+						},
+						{
+							title: "Promotion",
+							image: "/midsection/promotion.svg",
+						},
+					]}
+				/>
+				<MiddleSection
+					title="Why Partnership with us?"
+					content={[
+						{
+							title: "Adding value to Community",
+							image: "/midsection/value.svg",
+						},
+						{
+							title: "Promotion",
+							image: "/midsection/promotion.svg",
+						},
+						{
+							title: "Community Reach",
+							image: "/midsection/community-reach.svg",
+						},
+					]}
+				/>
+				<a
+					target="_blank"
+					className="px-4 py-3 bg-[#BE5038] text-white font-semibold transition-colors rounded-md hover:bg-opacity-80 xl:text-xl lg:text-xl md:text-lg"
+					rel="noreferrer"
+					href="https://forms.gle/ChwWq6JdxWjCeerg7"
+				>
+					Apply As Partner
+				</a>
+				<LeaderBoardSection />
+			</div>
+			<FooterSection />
+		</div>
+	);
 }
 
 export default App;
--- src/components/leaderboard.tsx
+++ src/components/leaderboard.tsx
@@ -1,11 +1,11 @@
 import leaderboard from "/leaderboard.svg";
 export default function LeaderBoardSection() {
-  return (
-    <div id="leaderboard" className="flex flex-col gap-4 justify-center items-center">
-      <p className="mid-section-title text-center text-lg mt-16 mb-12 sm:text-xl sm:mb-16 sm:mt-0 md:text-2xl lg:text-5xl lg:mt-16 lg:mb-20">
-        Global Leaderboard 2022
-      </p>
-      <img className="max-w-[800px] w-full sm:w-[80%]" src={leaderboard} alt="Leaderboard" />
-    </div>
-  );
+	return (
+		<div id="leaderboard" className="flex flex-col gap-4 justify-center items-center">
+			<p className="mid-section-title text-center text-lg mt-16 mb-12 sm:text-xl sm:mb-16 sm:mt-0 md:text-2xl lg:text-5xl lg:mt-16 lg:mb-20">
+				Global Leaderboard 2022
+			</p>
+			<img className="max-w-[800px] w-full sm:w-[80%]" src={leaderboard} alt="Leaderboard" />
+		</div>
+	);
 }
