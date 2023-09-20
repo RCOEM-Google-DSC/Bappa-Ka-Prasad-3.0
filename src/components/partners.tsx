f6dcb87e47c2556ad745c2699ed51290971976cf
EvolutionX <85353424+EvolutionX-10@users.noreply.github.com>
2023-09-20 22:55:04 +0530

    chore: oops

--- src/components/partners.tsx
+++ src/components/partners.tsx
@@ -15,9 +15,9 @@ export default function Partners() {
 	return (
 		<div className="w-full flex items-center p-12 justify-center">
 			<div className="max-w-[1150px] w-full flex flex-col gap-10 md:gap-16">
-				<div className="flex gap-3 justify-start items-center sm:justify-center">
+				<div className="flex gap-3 justify-center items-center sm:justify-center">
 					<img className="w-10 md:w-16" src="/prashad.svg" alt="prashad" />
-					<p className="mid-section-title text-lg sm:text-xl md:text-2xl lg:text-5xl">Our Partners</p>
+					<p className="mid-section-title text-5xl max-md:text-2xl">Our Partners</p>
 				</div>
 				<div className="w-full gap-8 gap-y-16 flex flex-wrap justify-between items-center md:px-8">
 					{partners.map((p, i) => {
